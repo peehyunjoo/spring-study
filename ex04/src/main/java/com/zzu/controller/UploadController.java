7e96e661bd2c8357eacf53f97d8ab119f0d8e4c3
peehyunjoo <peehyunjoo@gmail.com>
2018-01-06 18:30:32 +0900

    파일 및 이미지 업로드

--- ex04/src/main/java/com/zzu/controller/UploadController.java
+++ ex04/src/main/java/com/zzu/controller/UploadController.java
@@ -1,18 +1,29 @@
 package com.zzu.controller;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.UUID;
 
 import javax.annotation.Resource;
 
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+
+import com.zzu.util.MediaUtil;
+import com.zzu.util.UploadFileUtils;
 
 @Controller
 public class UploadController {
@@ -27,13 +38,14 @@ public class UploadController {
 	}
 	
 	@RequestMapping(value = "/uploadForm", method = RequestMethod.POST)
-	public void uploadForm(MultipartFile file, Model model) throws Exception {
+	public String uploadForm(MultipartFile file, Model model) throws Exception {
 		logger.info("originalName :" + file.getOriginalFilename());
 		logger.info("size : " + file.getSize());
 		logger.info("contentType : " +file.getContentType());
 		
 		String savedName  = uploadFile(file.getOriginalFilename(), file.getBytes());
 		model.addAttribute("savedName",savedName);
+		return "uploadResult";
 	}
 	
 	private String uploadFile(String originalName,byte[] fileData) throws Exception {
@@ -46,5 +58,56 @@ public class UploadController {
 		FileCopyUtils.copy(fileData, target);
 		
 		return savedName;
+	}
+	
+	@RequestMapping(value = "/uploadAjax", method = RequestMethod.GET)
+	public String uploadAjax() {
+		return "uploadAjax";
+	}
+	
+	@ResponseBody
+	@RequestMapping(value ="/uploadAjax",method = RequestMethod.POST, produces = "text/plain;charset=UTF-8")
+	public ResponseEntity<String> uploadAjax(MultipartFile file) throws Exception{
+		logger.info("originalName:" + file.getOriginalFilename());
+		logger.info("size:" + file.getSize());
+		logger.info("contentType:" + file.getContentType());
+		
+		return new ResponseEntity<String>(UploadFileUtils.uploadFile(uploadPath,file.getOriginalFilename(),file.getBytes()),HttpStatus.CREATED);
+	}
+	
+	@ResponseBody
+	@RequestMapping("/displayFile")
+	public ResponseEntity<byte[]> displayFile(String fileName)throws Exception{
+		InputStream in = null;
+		ResponseEntity<byte[]> entity = null;
+		
+		logger.info("FILE NAME :" + fileName);
+		
+		try {
+			String formatName = fileName.substring(fileName.lastIndexOf(".")+1);
+			
+			MediaType mType = MediaUtil.getMediaType(formatName);
+			
+			HttpHeaders headers = new HttpHeaders();
+			
+			in = new FileInputStream(uploadPath+fileName);
+			
+			if(mType !=null) {
+				headers.setContentType(mType);
+			}else {
+				fileName = fileName.substring(fileName.indexOf("_")+1);
+				headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+				headers.add("Content-Disposition","attachment;filename=\""+new String(fileName.getBytes("UTF-8"),"ISO-8859-1")+"\"");
+			}
+			
+			entity = new ResponseEntity<byte[]>(IOUtils.toByteArray(in),headers,HttpStatus.CREATED);
+		} catch (Exception e) {
+			e.printStackTrace();
+			entity = new ResponseEntity<byte[]>(HttpStatus.BAD_REQUEST);
+		}finally {
+			in.close();
+		}
+		
+		return entity;
 	}
 }
--- ex04/src/main/webapp/WEB-INF/views/uploadForm.jsp
+++ ex04/src/main/webapp/WEB-INF/views/uploadForm.jsp
@@ -3,13 +3,28 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=EUC-KR">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>Insert title here</title>
+<style>
+	iframe{
+		width:0px;
+		height:0px;
+		border:0px;
+	}
+</style>
+<script>
+	function addFilePath(msg){
+		alert(msg);
+		document.getElementById("form1").reset();
+	}
+</script>
 </head>
 <body>
-	<form action = "uploadForm" method="post" id = "form1" enctype = "multipart/form-data">
+	<form action = "uploadForm" method="post" id = "form1" enctype = "multipart/form-data" target="zeroFrame">
 		<input type ="file" name="file"/>
 		<input type = "submit">
 	</form>
+	
+	<iframe name="zeroFrame"></iframe>
 </body>
 </html>
